16bb77e0223bf567408a5278fa194f552032fab6
SAB <sevanboyadjian@gmail.com>
2022-05-28 16:32:25 -0400

    add additional files, tested out html

---
+++ client/components/Header/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+    return (
+
+        <header className="header-class">
+            <p>
+                HEADER
+            </p>
+        </header>
+    );
+}
+
+export default Header;
