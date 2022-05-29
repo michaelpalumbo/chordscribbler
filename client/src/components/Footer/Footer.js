b4acd46b0a723a1c26b5042450f7b7c472409184
Michael Palumbo <michaelpalumbo@users.noreply.github.com>
2022-05-28 21:49:18 -0400

    npm run develop now launches fullstack MERN

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return (
 
         <footer className="footer-class"> 
-      <img className="footer-img" src={placeholder} alt="" />
+      <img className="footer-img" src={"placeholder"} alt="" />
         </footer>
     );
 }
