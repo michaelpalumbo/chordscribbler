d330efd274a38f7338d00adf914cc5cea93819e6
Echeta Okeke <echeta@rogers.com>
2022-06-02 17:36:00 -0400

    stuff

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -290,7 +290,7 @@ const Home = () =>{
               </div>
               <div className="col-md-4 alert alert-secondary" role="alert">
                  {/* History panel */}
-                 <p class="text-center"><h4 class="alert-heading">Your History</h4></p>
+                 <p class="text-center"><h4 class="alert-heading">{username}'s History</h4></p>
                   
                   <textarea readonly className="form-control" id="chordHistory" placeholder="Your progress will be listed here" rows="30" defaultValue={historyPanel}></textarea>
             
